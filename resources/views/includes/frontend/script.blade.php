d758634619d2246f2d528987390a81d556255ad7
teddinata <teddinata@users.noreply.github.com>
2024-01-05 17:31:24 +0700

    fix: google track data-

--- resources/views/includes/frontend/script.blade.php
+++ resources/views/includes/frontend/script.blade.php
@@ -10,3 +10,12 @@
  <script src="{{ url('frontend/js/owl.carousel.min.js') }}"></script>
  <script src="{{ url('frontend/js/main.js') }}"></script>
  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2NWZHQKSHN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2NWZHQKSHN');
+</script>
